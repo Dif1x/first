f4706e9d7af6066b92fcf1d4250f4c7d60f77e4e
Dif1x <egoroluskin@gmail.com>
2026-07-03 16:23:33 +0300

    Init

---
+++ chetnost`.py
@@ -0,0 +1,16 @@
+print("Введите 3 числа: ")
+a = int(input())
+b = int(input())
+c = int(input())
+if a % 2 == 0:
+    print(f"{a} - четное")
+else:
+    print(f"{a} - нечетное")
+if b % 2 == 0:
+    print(f"{b} - четное")
+else:
+    print(f"{b} - нечетное")
+if c % 2 == 0:
+    print(f"{c} - четное")
+else:
+    print(f"{c} - нечетное")
